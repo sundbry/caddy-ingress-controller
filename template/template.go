271641013df06c6eaa6aeb4b85b38965503afb9d
Jason Hutchinson <zikes@zikes.me>
2017-04-30 14:55:16 -0500

    add template controller

---
+++ template/template.go
@@ -0,0 +1,125 @@
+package template
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	text_template "text/template"
+
+	"github.com/golang/glog"
+
+	"git.zikes.me/caddy-ingress/config"
+
+	"k8s.io/ingress/core/pkg/ingress"
+	"k8s.io/ingress/core/pkg/watch"
+)
+
+const (
+	slash         = "/"
+	defBufferSize = 65535
+	errNoChild    = "wait: no child processes"
+)
+
+// Template
+type Template struct {
+	tmpl    *text_template.Template
+	fw      watch.FileWatcher
+	s       int
+	tmplBuf *bytes.Buffer
+}
+
+// NewTemplate returns a new Template instance or an
+// error if the specified template contains errors
+func NewTemplate(file string, onChange func()) (*Template, error) {
+	tmpl, err := text_template.New("caddy.tmpl").Funcs(funcMap).ParseFiles(file)
+	if err != nil {
+		return nil, err
+	}
+	fw, err := watch.NewFileWatcher(file, onChange)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Template{
+		tmpl:    templ,
+		fw:      fw,
+		s:       defBufferSize,
+		tmplBuf: bytes.NewBuffer(make([]byte, 0, defBufferSize)),
+	}, nil
+}
+
+// Close removes the file watcher
+func (t *Template) Close() {
+	t.fw.Close()
+}
+
+// Write populates a buffer using the template with the Caddy configuration
+// and the servers and upstreams created by the Ingress rules
+func (t *Template) Write(conf config.TemplateConfig) ([]byte, error) {
+	defer t.tmplBuf.Reset()
+
+	defer func() {
+		if t.s < t.tmplBuf.Cap() {
+			glog.V(2).Infof("adjusting template buffer size from %v to %v", t.s, t.tmplBuf.Cap())
+			t.s = t.tmplBuf.Cap()
+			t.tmplBuf = bytes.NewBuffer(make([]byte, 0, t.tmplBuf.Cap()))
+		}
+	}()
+
+	if glog.V(3) {
+		b, err := json.Marshal(conf)
+		if err != nil {
+			glog.Errorf("unexpected error: %v", err)
+		}
+		glog.Infof("Caddy configuration: %v", string(b))
+	}
+
+	err := t.tmpl.Execute(t.tmplBuf, conf)
+	if err != nil && err.Error() != errNoChild {
+		return nil, err
+	}
+
+	return t.tmplBuf.Bytes(), nil
+}
+
+var (
+	funcMap = text_template.FuncMap{
+		"empty": func(input interface{}) {
+			check, ok := input.(string)
+			if ok {
+				return len(check) == 0
+			}
+			return true
+		},
+		"buildLocation":            buildLocation,
+		"buildAuthLocation":        buildAuthLocation,
+		"buildAuthResponseHeaders": buildAuthResponseHeaders,
+		"buildProxyPass":           buildProxyPass,
+		"buildRateLimitZones":      buildRateLimitZones,
+		"buildRateLimit":           buildRateLimit,
+		"isLocationAllowed":        isLocationAllowed,
+		"buildLogFormatUpstream":   buildLogFormatUpstream,
+		"contains":                 strings.Contains,
+		"hasPrefix":                strings.HasPrefix,
+		"hasSuffix":                strings.HasSuffix,
+		"toUpper":                  strings.toUpper,
+		"toLower":                  strings.toLower,
+	}
+)
+
+// buildLocation produces the location string, if the ingress has redirects
+// (specified through the ingress.kubernetes.io/rewrite-target annotation)
+func buildLocation(input interface{}) string {
+	location, ok := input.(*ingress.Location)
+	if !ok {
+		return slash
+	}
+
+	path := location.Path
+	if len(location.Redirect.Target) > 0 && location.Redirect.Target != path {
+		if path == slash {
+			return fmt.Sprintf("%s", path)
+		}
+	}
+	return path
+}
