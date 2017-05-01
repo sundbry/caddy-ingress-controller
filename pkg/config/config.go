29ce51701c73c85c8bf7901d319e57b6a27b463e
Kevin Minehart <kminehart@wehco.com>
2017-05-01 16:57:56 -0500

    Add basic configs.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,6 +2,44 @@
 
 package config
 
-import ()
+import (
+	"k8s.io/ingress/core/pkg/ingress"
+	"k8s.io/ingress/core/pkg/ingress/defaults"
+)
 
-const ()
+const (
+	// bodySize = "1m"
+
+	// `log stdout`
+	// Send all logs to stdout
+	logLocation = "stdout"
+)
+
+var (
+	SSLDirectory = "/root/.caddy"
+)
+
+// Configuration represents the content of /etc/Caddyfile
+type Configuration struct {
+	defaults.Backend `json:",squash"`
+	LogLocation      string
+}
+
+func NewDefault() Configuration {
+	cfg := Configuration{
+		LogLocation: logLocation,
+	}
+
+	return cfg
+}
+
+// TemplateConfig contains the configuration to render the file /etc/Caddyfile
+type TemplateConfig struct {
+	Backends      []*ingress.Backend
+	Servers       []*ingress.Server
+	TCPBackends   []ingress.L4Service
+	UDPBackends   []ingress.L4Service
+	HealthzURI    string
+	Cfg           Configuration
+	IsIPV6Enabled bool
+}
