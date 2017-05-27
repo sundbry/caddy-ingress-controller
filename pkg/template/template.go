ff6db3dc359dd71fe5962f323e268f1dbcbc46d8
Jason Hutchinson <zikes@zikes.me>
2017-05-27 03:37:53 -0500

    remove unused template functions

--- pkg/template/template.go
+++ pkg/template/template.go
@@ -6,14 +6,11 @@ package template
 
 import (
 	"bytes"
-	"fmt"
 	"log"
 	text_template "text/template"
 
 	"git.nwaonline.com/kubernetes/caddy-ingress/pkg/config"
 
-	api "k8s.io/client-go/pkg/api/v1"
-	"k8s.io/ingress/core/pkg/ingress"
 	"k8s.io/ingress/core/pkg/watch"
 )
 
@@ -76,70 +73,6 @@ func (t *Template) Write(conf config.TemplateConfig) ([]byte, error) {
 
 var (
 	funcMap = text_template.FuncMap{
-		"empty": func(input interface{}) bool {
-			check, ok := input.(string)
-			if ok {
-				return len(check) == 0
-			}
-			return true
-		},
-		"buildLocation": buildLocation,
 		"cleanHostname": cleanHostname,
-		"fromSpec":      fromSpec,
 	}
 )
-
-func fromSpec(input interface{}) string {
-	spec, ok := input.(api.ServiceSpec)
-	if !ok {
-		return ""
-	}
-	switch spec.Type {
-	case "ClusterIP":
-		// The ClusterIP ServiceType means the service has been assigned a
-		// dedicated unique IP address within the server
-		return spec.ClusterIP
-	case "NodePort":
-		// The NodePort ServiceType means the service is given a dedicated
-		// unique port on every node in the cluster, which is provided by the
-		// spec's ClusterIP
-		return spec.ClusterIP
-	case "LoadBalancer":
-		// The LoadBalancer ServiceType creates a dedicated Load Balancer IP
-		// on supported cloud providers
-		return spec.LoadBalancerIP
-	case "ExternalName":
-		// ExternalName is the external reference that kubedns or equiv
-		// will return as a CNAME record for this service
-		return spec.ExternalName
-	}
-	return ""
-}
-
-// buildLocation produces the location string, if the ingress has redirects
-// (specified through the ingress.kubernetes.io/rewrite-target annotation)
-func buildLocation(input interface{}) string {
-	location, ok := input.(*ingress.Location)
-	if !ok {
-		return slash
-	}
-
-	path := location.Path
-	if len(location.Redirect.Target) > 0 && location.Redirect.Target != path {
-		if path == slash {
-			return fmt.Sprintf("%s", path)
-		}
-	}
-	return path
-}
-
-// cleanHostname will replace the "_" hostname with ""
-func cleanHostname(input interface{}) string {
-	if hostname, ok := input.(string); ok {
-		if hostname == "_" {
-			return ""
-		}
-		return hostname
-	}
-	return ""
-}
